57f6c2b53837a089d4e8736bf95dcb440300e241
GreenGoblin01002 <erlhal01002@stud.noroff.no>
2025-01-28 13:49:51 +0100

    hello

---
+++ src/HelloWorld.js
@@ -0,0 +1,11 @@
+// src/HelloWorld.js
+import React from "react";
+
+const HelloWorld = () => {
+  return (
+    <div>
+      <h1>Hello, React World!</h1>
+    </div>
+  );
+};
+export default HelloWorld;
